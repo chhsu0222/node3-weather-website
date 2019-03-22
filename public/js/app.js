bf440f98c1b96df8a42ba66f6f5e32a82ed4f657
ch <ch@mail.startek-eng.com>
2019-03-22 14:08:08 +0800

    sec. 8, video 58, Creating a search form.
    event.preventDelault() is going to prevent that default behavior
    which is to refresh the browser, allowing the server to render a
    new page. Instead, it's going to do nothing. It's just going to
    allow us to do whatever we want by letting the function run.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=kaohsiang').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(data.error)
-        }
-        console.log(data.location)
-        console.log(data.forecast)
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const url = 'http://localhost:3000/weather?address=' + search.value
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.log(data.error)
+            }
+            console.log(data.location)
+            console.log(data.forecast)
+        })
     })
 })
