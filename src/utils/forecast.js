e6c0cbffea2297746d5409b7bd3b4767c905a581
ch <ch@mail.startek-eng.com>
2019-03-22 17:25:16 +0800

    Using SI units (Celsius) for forecasting.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const url = `https://api.darksky.net/forecast/1a5e6c07f1fb77d34bdae5ba084e3168/${lat},${long}`
+    const url = `https://api.darksky.net/forecast/1a5e6c07f1fb77d34bdae5ba084e3168/${lat},${long}?units=si`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
         } else {
             const currently = body.currently
             const summary = body.daily.data[0].summary
-            callback(undefined, `${summary} It is currently ${currently.temperature} degree out. There is a ${currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${summary} It is currently ${currently.temperature} degree Celsius out. There is a ${currently.precipProbability}% chance of rain.`)
         }
     })
 }
