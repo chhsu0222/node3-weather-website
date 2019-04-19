5db8b0c23104a9e202ee6810ccfc67dc32681766
ch <ch@mail.startek-eng.com>
2019-04-19 11:43:44 +0800

    Update geocode to support chinese search.

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,8 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
+    // For the cases of chinese input
+    address = encodeURIComponent(address)
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiY2hoc3UiLCJhIjoiY2p0NTN3N3hjMDJldzQzcXh6bzJzczkycSJ9.UwfzfQ2ukbEZBkd7Mwmdcw&limit=1`
 
     request({ url, json: true }, (error, geoData) => {
